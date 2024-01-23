2d1de28e47041d1fdff849132f9c9d778295d753
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-01-23 15:02:47 +0700

    init projects

---
+++ server-project/src/test/java/com/example/serverproject/ServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
